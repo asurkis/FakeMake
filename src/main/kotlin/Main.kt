bfe6b769752600656b80ccc801a1412c4c0ed40a
Anton Surkis <antonsurkis@gmail.com>
2022-10-23 10:57:14 +0300

    Add graph traverser and file checker

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,51 @@
 import com.charleskorn.kaml.Yaml
 import kotlinx.serialization.*
+import java.io.File
+import java.io.FileNotFoundException
+import kotlin.system.exitProcess
 
 @Serializable
-data class CommandRecord(val dependencies: Array<String>, val target: String, val run: String)
+data class Target(val dependencies: List<String>, val target: String, val run: String) {
+    fun satisfy(allTargets: Map<String, Target>): Boolean {
+        var updateRequired = false
+        val targetFile = File(target)
+        for (key in dependencies) {
+            updateRequired = allTargets[key]?.satisfy(allTargets) ?: satisfyFile(targetFile, key) || updateRequired
+        }
+        if (updateRequired) {
+            println(run)
+        }
+        return updateRequired
+    }
 
-fun main() {
+    fun satisfyFile(targetFile: File, name: String): Boolean {
+        val f = File(name)
+        if (!f.exists()) {
+            throw FileNotFoundException("Required file not found: $name")
+        }
+        return !targetFile.exists() || targetFile.lastModified() < f.lastModified()
+    }
+}
+
+fun main(/*args: Array<String>*/) {
     val input = """
         compile:
           dependencies:
-          - "main.c"
-          target: "main.o"
-          run: "gcc -c main.c -o main.o"
+          - main.c
+          target: main.o
+          run: gcc -c main.c -o main.o
         build:
           dependencies:
-          - "compile"
-          target: "main"
-          run: "gcc main.o -o main"
+          - compile
+          target: main
+          run: gcc main.o -o main
     """.trimIndent()
-    println(input)
-    val targets: Map<String, CommandRecord> = Yaml.default.decodeFromString(input)
-    println(Yaml.default.encodeToString(targets))
+
+    val allTargets: Map<String, Target> = Yaml.default.decodeFromString(input)
+    try {
+        println(allTargets["build"]?.satisfy(allTargets))
+    } catch (e: FileNotFoundException) {
+        System.err.println(e.message)
+        exitProcess(1)
+    }
 }
