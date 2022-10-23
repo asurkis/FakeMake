4b30ac3569568a78dfa23970692a7c9cc2401e4e
Anton Surkis <antonsurkis@gmail.com>
2022-10-23 13:19:13 +0300

    Add Jar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ repositories {
 
 dependencies {
     implementation("com.charleskorn.kaml:kaml:0.49.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.4.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation(kotlin("test"))
 }
 
@@ -29,4 +31,13 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("ru.itmo.asurkis.test.fakemake.MainKt")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = application.mainClass
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) { it } else { zipTree(it) }})
 }
